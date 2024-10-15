e07a49dbca6e0838634a45d5784766a64b3c30f6
luzk-mb <mejiakarine20@gmail.com>
2024-10-15 13:56:59 -0500

    script3

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Validación de formulario
+function mostrarMensaje(){
+    alert("HOLA!!!, Bienvenidos...");
+}
+/*validacion de formulario*/
 const form = document.getElementById('contactForm');
-form.addEventListener('submit', 
-function(event) {
- const name = document.getElementById('name').value;
- const email = document.getElementById('email').value;
- if (name === '' || email === '') {
- alert('Por favor, completa todos los campos.');
- event.preventDefault();
- } else {
- alert('¡Formulario enviado correctamente!');
- }
+
+form.addEventListener('submit', function(event){
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+
+    if (nombre === '' || email === '') {
+        alert('Por favor, completa todos los campos.');
+        event.preventDefault();
+    } else {
+        alert('¡Formulario enviado correctamente!');
+    }
 });
