caa124895d55765c61f09e6da790509438da5c74
luzk-mb <mejiakarine20@gmail.com>
2024-10-15 13:33:05 -0500

    validar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function mostrarMensaje(){
     alert(HOLA!!!, Bienvenidos...);
 }
+// Validación de formulario
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', 
+function(event) {
+ const name = document.getElementById('name').value;
+ const email = document.getElementById('email').value;
+ if (name === '' || email === '') {
+ alert('Por favor, completa todos los campos.');
+ event.preventDefault();
+ } else {
+ alert('¡Formulario enviado correctamente!');
+ }
+});
